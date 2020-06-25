5afd65dd0cb7f6c8bde58688a8930c548af19094
Nana <nanayaw.net@live.com>
2020-06-25 16:16:03 -0400

    Added dynamodb on docker practice code

---
+++ ddb_docker/main.py
@@ -0,0 +1,18 @@
+import os
+import boto3
+
+#os.environ['AWS_PROFILE'] = "default"
+#os.environ['AWS_DEFAULT_REGION'] = "us-east-1"
+
+# For a Boto3 service resource ('resource' is for higher-level, abstracted access to Dynamo)
+dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:8000')
+
+table = dynamodb.Table('Movies')
+response = table.get_item(Key={'year': 2015, 'title': 'The Big New Movie'})
+
+print (response)
+print ('end response')
+print(list(dynamodb.tables.all()))
+
+print("Hello ending...")
+
